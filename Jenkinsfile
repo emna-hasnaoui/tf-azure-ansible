037f0597f396dc6d4af9b9b4c7f7f25c0f025f38
emna <emna.hasnaoui@insat.ucar.tn>
2025-11-21 15:16:24 +0100

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,37 +2,37 @@ pipeline {
     agent any
 
     environment {
-        ARM_SUBSCRIPTION_ID = 'f4042894-a707-4691-a7c5-9c34c95bbc87'
-        ARM_TENANT_ID       = 'dbd6664d-4eb9-46eb-99d8-5c43ba153c61'
+        ARM_CLIENT_ID       = credentials('AZURE_SP_CREDENTIAL_USR')
+        ARM_CLIENT_SECRET   = credentials('AZURE_SP_CREDENTIAL_PSW')
+
+        ARM_TENANT_ID       = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
+        ARM_SUBSCRIPTION_ID = "f4042894-a707-4691-a7c5-9c34c95bbc87"
     }
 
     stages {
 
-        stage('Terraform Pipeline') {
+        stage('Terraform Init') {
             steps {
-                withCredentials([
-                    usernamePassword(
-                        credentialsId: 'AZURE_SP_CREDENTIAL',
-                        usernameVariable: 'ARM_CLIENT_ID',
-                        passwordVariable: 'ARM_CLIENT_SECRET'
-                    )
-                ]) {
+                sh '''
+                    echo "Initializing Terraform..."
+                    terraform init
+                '''
+            }
+        }
 
-                    echo "Credentials loaded"
+        stage('Terraform Plan') {
+            steps {
+                sh '''
+                    terraform plan -out=tfplan
+                '''
+            }
+        }
 
-                    bat 'terraform init'
-
-                    bat """
-                    terraform plan ^
-                      -var client_id=%ARM_CLIENT_ID% ^
-                      -var client_secret=%ARM_CLIENT_SECRET% ^
-                      -var tenant_id=%ARM_TENANT_ID% ^
-                      -var subscription_id=%ARM_SUBSCRIPTION_ID% ^
-                      -out=tfplan
-                    """
-
-                    bat "terraform apply -auto-approve tfplan"
-                }
+        stage('Terraform Apply') {
+            steps {
+                sh '''
+                    terraform apply -auto-approve tfplan
+                '''
             }
         }
     }
--- main.tf
+++ main.tf
@@ -2,80 +2,77 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "~>3.0"
     }
   }
+
+  backend "local" {} # Change to "azurerm" if you later want remote backend
 }
 
 provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-tp-devops"
-  location = "France Central"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  name     = "rg-jenkins-tf"
+  location = "East US"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-tp"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "vnet-jenkins"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-tp"
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "pip-tp"
-  resource_group_name = azurerm_resource_group.rg.name
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vm-public-ip"
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
 }
 
+# NIC
 resource "azurerm_network_interface" "nic" {
-  name                = "nic-tp"
+  name                = "vm-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "nicConfig"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
+# Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-tp"
-  location            = azurerm_resource_group.rg.location
+  name                = "jenkins-tf-vm"
   resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
+
+  admin_username = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.nic.id
+    azurerm_network_interface.nic.id,
   ]
 
-  admin_password                   = "Azure12345!!"
-  disable_password_authentication  = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")  # or replace with your key path
   }
 
   source_image_reference {
@@ -84,8 +81,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-}
-
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
 }
